df5deacd1c13573b21cf229e118b2eaaac0f272d
herbertbrgs1-cyber <herbertbrgs1@gmail.com>
2025-09-26 10:35:03 -0300

    att

--- USO DO LOCALSTORAGE/scripts.js
+++ USO DO LOCALSTORAGE/scripts.js
@@ -1,87 +1,84 @@
-function salvarFuncionario(e) {
-  e.preventDefault();
+function cadastrarFuncionario(e) {
+    const id = document.getElementById("campo-id").value
+    const nome = document.getElementById("campo-nome").value
+    const salario = document.getElementById("campo-salario").value
+    const cargo = document.getElementById("campo-cargo").value
+    const departamento = document.getElementById("campo-departamento").value
 
-  const funcionario = {
-    nome: document.getElementById("nome").value,
-    idade: document.getElementById("idade").value,
-    cargo: document.getElementById("cargo").value,
-    salario: document.getElementById("salario").value,
-    departamento: document.getElementById("departamento").value,
-  };
+    const novoFuncionario = {
+        "id": id,
+        "nome": nome,
+        "salario": salario,
+        "cargo": cargo,
+        "departamento": departamento
+    }
 
-  let funcionarios = JSON.parse(localStorage.getItem("funcionarios")) || [];
-  let editIndex = document.getElementById("editIndex").value;
+    const funcionarios = JSON.parse(localStorage.getItem("funcionarios")) || []
 
-  if (editIndex === "") {
-    
-    funcionarios.push(funcionario);
-  } else {
-    
-    funcionarios[editIndex] = funcionario;
-    document.getElementById("editIndex").value = "";
-  }
+    const funcionarioJaExiste = funcionarios.find((f) => f.id == novoFuncionario.id)
 
-  localStorage.setItem("funcionarios", JSON.stringify(funcionarios));
+    if (funcionarioJaExiste) {
+        alert("Já existe funcionário com esse id!")
+        return
+    }
 
-  carregarFuncionarios();
-  e.target.reset();
+    funcionarios.push(novoFuncionario)
+
+    localStorage.setItem("funcionarios", JSON.stringify(funcionarios))
 }
 
-function carregarFuncionarios() {
-  const tabela = document.querySelector("#tabela-funcionarios tbody");
-  tabela.innerHTML = "";
+//Criar a função renderizarTabela:
+// 1 - Capturar informação do localStorage(getItem)
+// 2 - Utilizar forEach para percorrer cada elemento da lista do localStorage
+// 3 - Dentro do forEach criar um elemento <tr></tr> e dentro desse elemento criar os elementos <td></td> para cada informação do item que deseja exibir
+// 4 - Adicionar os elementos criados ao tbody da tabela de exibição
 
-  let funcionarios = JSON.parse(localStorage.getItem("funcionarios")) || [];
+// Criar a funcionalidade editar:
+// No campo Ação, todos os itens devem conter um botão (<a></a>) de Editar
+// Ao clicar em Editar, o usuário deve ser redirecionado a uma nova tela, com um formulário preenchido pelas informações do item escolhido 
+// Ao final do formulário deve conter um botão "Salvar Alterações" e as mundanças feitas no item devem ser armazenadas no localStorage
+// As mudanças devem refletir na tabela da página original
 
-  funcionarios.forEach((f, index) => {
-    let tr = document.createElement("tr");
+function renderizarTabela() {
 
-    tr.innerHTML = `
-      <td>${f.nome}</td>
-      <td>${f.idade}</td>
-      <td>${f.cargo}</td>
-      <td>${f.salario}</td>
-      <td>${f.departamento}</td>
-      <td>
-        <button class="ver-btn" onclick="mostrarFuncionario(${index})">Ver</button>
-        <button class="editar-btn" onclick="editarFuncionario(${index})">Editar</button>
-        <button class="remover-btn" onclick="removerFuncionario(${index})">Remover</button>
-      </td>
-    `;
+    const funcionarios = JSON.parse(localStorage.getItem("funcionarios")) || []
+    const corpoTabelaHtml = document.querySelector("#tabela-funcionarios tbody")
 
-    tabela.appendChild(tr);
-  });
+    funcionarios.forEach(funcionario => {
+        const linha = document.createElement("tr")
+        linha.innerHTML = `
+        <td>${funcionario.id}</td>
+        <td>${funcionario.nome}</td>
+        <td>${Number(funcionario.salario).toLocaleString("pt-BR", { style: "currency", currency: "BRL" })}</td>
+        <td>${funcionario.cargo}</td>
+        <td>${funcionario.departamento}</td>
+        
+        `
+
+        const campoAcoes = document.createElement("td")
+        campoAcoes.classList.add("acoes")
+
+        const botaoRemover = document.createElement("button")
+        botaoRemover.type = "button"
+        botaoRemover.innerText = "REMOVER"
+
+        botaoRemover.addEventListener("click", () => {
+            const listaFuncionariosAtual = JSON.parse(localStorage.getItem("funcionarios")) || []
+
+            const listaFuncionariosFiltrada = listaFuncionariosAtual.filter((f) => f.id != funcionario.id)
+
+
+            localStorage.setItem("funcionarios", JSON.stringify(listaFuncionariosFiltrada))
+            window.location.reload()
+        })
+
+        campoAcoes.appendChild(botaoRemover)
+        linha.appendChild(campoAcoes)
+
+
+        corpoTabelaHtml.appendChild(linha)
+    });
+
 }
 
-function mostrarFuncionario(index) {
-  let funcionarios = JSON.parse(localStorage.getItem("funcionarios")) || [];
-  let f = funcionarios[index];
-  alert(
-    `Nome: ${f.nome}\nIdade: ${f.idade}\nCargo: ${f.cargo}\nSalário: ${f.salario}\nDepartamento: ${f.departamento}`
-  );
-}
-
-function editarFuncionario(index) {
-  let funcionarios = JSON.parse(localStorage.getItem("funcionarios")) || [];
-  let f = funcionarios[index];
-
-  document.getElementById("nome").value = f.nome;
-  document.getElementById("idade").value = f.idade;
-  document.getElementById("cargo").value = f.cargo;
-  document.getElementById("salario").value = f.salario;
-  document.getElementById("departamento").value = f.departamento;
-  document.getElementById("editIndex").value = index;
-}
-
-function removerFuncionario(index) {
-  let funcionarios = JSON.parse(localStorage.getItem("funcionarios")) || [];
-  funcionarios.splice(index, 1); 
-  localStorage.setItem("funcionarios", JSON.stringify(funcionarios));
-  carregarFuncionarios(); 
-}
-
-function cancelarEdicao() {
-  document.getElementById("editIndex").value = "";
-}
-
-window.onload = carregarFuncionarios;
+renderizarTabela()
